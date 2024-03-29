90300ff218e69fad725e57ace911bd16564e4dbf
milos-majkic <milos1994majkic@gmail.com>
2024-03-29 13:58:27 +0100

    Added email services

--- src/main/java/rs/ac/uns/ftn/springsecurityexample/repository/RatingRepository.java
+++ src/main/java/rs/ac/uns/ftn/springsecurityexample/repository/RatingRepository.java
@@ -14,7 +14,8 @@ public interface RatingRepository extends JpaRepository<Rating, Long> {
     @Query("SELECT AVG(r.value) FROM Rating r WHERE r.doctor.id = ?1")
     float getAverageForDoctor(long doctorId);
 
-    @Query("SELECT COUNT(r) > 0 FROM Rating r WHERE r.user.id = :userId AND (r.doctor.id = :doctorId OR r.clinic.id = :clinicId)")
+    @Query("SELECT COUNT(r) > 0 FROM Rating r WHERE r.user.id = :userId AND " +
+            "(r.doctor.id = :doctorId OR r.clinic.id = :clinicId)")
     boolean existsRatingByUserAndDoctorOrClinic(@Param("userId") long userId, @Param("doctorId") long doctorId, @Param("clinicId") long clinicId);
 
 }
--- src/main/java/rs/ac/uns/ftn/springsecurityexample/service/impl/EmailService.java
+++ src/main/java/rs/ac/uns/ftn/springsecurityexample/service/impl/EmailService.java
@@ -7,9 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import rs.ac.uns.ftn.springsecurityexample.model.Appointment;
 import rs.ac.uns.ftn.springsecurityexample.model.User;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -19,7 +24,6 @@ public class EmailService {
 
 	@Autowired
 	private Environment env;
-
 
 	public void sendActivationCode(User user) {
 
@@ -60,5 +64,74 @@ public class EmailService {
 		return Base64.getUrlEncoder().withoutPadding().encodeToString(uniqueCodeBytes);
 	}
 
-	
+	public void sendAppointmentConfiramtion(User user) {
+
+		SimpleMailMessage mail = new SimpleMailMessage();
+		mail.setTo(user.getEmail());
+		mail.setFrom(env.getProperty("spring.mail.username"));
+		mail.setSubject("Appointment confirmation");
+
+		mail.setText("Hello, " + user.getFirstName() + "!\n\n"
+				+ "We are pleased to inform you that your appointment has been successfully booked.\n"
+				+ "We look forward to seeing you at your scheduled appointment.\n\n"
+				+ "Best regards,");
+
+		javaMailSender.send(mail);
+	}
+
+	public void sendAppointmentInquiry(User user, Appointment appointment){
+		MimeMessage message = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+		try {
+			helper.setTo(user.getEmail());
+			helper.setFrom(env.getProperty("spring.mail.username"));
+			helper.setSubject("Appointment Inquiry");
+			String content = getHtmlContent(user, appointment);
+			helper.setText(content, true);
+			javaMailSender.send(message);
+		} catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+	public String getHtmlContent(User user, Appointment appointment) {
+		return "<!DOCTYPE html>\n" +
+				"<html lang=\"en\">\n" +
+				"<head>\n" +
+				"    <meta charset=\"UTF-8\">\n" +
+				"    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+				"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+				"    <title>Email Template</title>\n" +
+				"    <style>\n" +
+				"        /* Button styles */\n" +
+				"        .button {\n" +
+				"            display: inline-block;\n" +
+				"            padding: 10px 20px;\n" +
+				"            text-decoration: none;\n" +
+				"            color: #fff;\n" +
+				"            border-radius: 5px;\n" +
+				"            transition: background-color 0.3s ease;\n" +
+				"        }\n" +
+				"        .confirm {\n" +
+				"            background-color: #007bff;\n" +
+				"        }\n" +
+				"        .deny {\n" +
+				"            background-color: #dc3545;\n" +
+				"        }\n" +
+				"        .button:hover {\n" +
+				"            background-color: #0056b3;\n" +
+				"        }\n" +
+				"    </style>\n" +
+				"</head>\n" +
+				"<body>\n" +
+				"    <p>Dear " + user.getFirstName() + ",</p>\n" +
+				"    <p>We would like to confirm or deny your appointment scheduled for " + appointment.getDate() + " at " + appointment.getTime() + ".</p>\n" +
+				"    <p>Please click one of the buttons below to confirm or deny the appointment:</p>\n" +
+				"    <a href=\"http://localhost:5173/appointmentInquiryRejected/" + appointment.getId() + "\" class=\"button deny\">Deny</a>\n" +
+				"    <a href=\"http://localhost:5173/appointmentInquiryAccepted/" + appointment.getId() + "\" class=\"button confirm\">Confirm</a>\n" +
+				"    <p>Best regards,</p>\n" +
+				"</body>\n" +
+				"</html>";
+	}
 }
